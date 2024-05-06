49c169c9fb0ab03956eec857ae15ca1bcc5ee239
osuke-noguchi <noguchi.nodai@gmail.com>
2024-05-07 06:50:53 +0900

    layouts

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,15 @@
 <template>
   <div>This page is about me.</div>
+  <!-- <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div> -->
 </template>
+
+<script setup>
+// definePageMeta({
+//   layout: "another",
+// });
+// function enableCustomLayout() {
+//   setPageLayout("another");
+// }
+</script>
